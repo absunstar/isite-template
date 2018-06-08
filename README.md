ddfdf2953f513517c228366b119b6684324a36b4
absunstar <absnstar@gmail.com>
2018-06-08 11:21:29 +0200

    #

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Template Site For Isite Framework
+
+## how to install
+
+```sh
+git clone https://github.com/absunstar/isite-template
+npm run init
+```
+
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const site = require('isite')({
     port: 8080
 })
 
-
 site.get({name : '/js' , path : site.dir + '/js' , parser : 'js'})
 site.get({name : '/' , path : site.dir + '/html/index.html' , parser : 'html css js'})
 
