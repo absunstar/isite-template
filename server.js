05fbed5208cd8b726e7e9451a3678f160f7f7ad7
absunstar <absnstar@gmail.com>
2018-06-08 11:18:28 +0200

    #

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const site = require('../../isite')({
+const site = require('isite')({
     port: 8080
 })
 
-site.importApp(__dirname + '/../isite-client')
-site.importApp(__dirname + '/../isite-security')
 
 site.get({name : '/js' , path : site.dir + '/js' , parser : 'js'})
 site.get({name : '/' , path : site.dir + '/html/index.html' , parser : 'html css js'})
